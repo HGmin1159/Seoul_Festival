63fc516c8c3386d90fc97f7592385d950ed02513
Hokyeong Choi <dollhy.gmail.com>
2019-10-29 19:21:34 +0900

    Search

--- experiment/tojson.py
+++ experiment/tojson.py
@@ -2,26 +2,32 @@ import json
 res = []
 temp = []
 seen = False
-with open("A-team/experiment/festag16.txt", encoding="utf-8") as f:
+pathfrom = "experiment/festag19.csv"
+pathto = "experiment/festag19.json"
+with open(pathfrom, encoding="utf-8") as f:
     headers = list(map(lambda x: x.rstrip(), f.readline().split(',')))[1:]
     for line in f:
         if '"' in line:
             seen = not seen
         if not seen:
             if temp:
-                temp.append(line.rstrip())
-                line = ''.join(temp)
-                line = list(map(lambda x: x.rstrip(), line.split(',')))[1:]
+                temp.append(line.strip())
+                line = ' '.join(temp)
+                line = list(map(lambda x: x.strip().strip('"'), line.split(',')))[1:]
                 res.append(dict(zip(headers, line)))
                 temp = []
             else:
-                line = list(map(lambda x: x.rstrip(), line.split(',')))[1:]
+                line = list(map(lambda x: x.strip().strip('"'), line.split(',')))[1:]
                 res.append(dict(zip(headers, line)))
         else:
-            temp.append(line.rstrip())
+            temp.append(line.strip())
 
-with open("A-team/experiment/festag16.json", 'w', encoding="utf-8") as f:
+with open(pathto, 'w', encoding="utf-8") as f:
     f.write(json.dumps(res, ensure_ascii=False))
        
+# for i in res:
+#     if '"' in i["축제명"]:
+#         print(i, '\n')
+
 # for i in res:
 #     print(i, '\n')
