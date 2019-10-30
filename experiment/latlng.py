f8384e142e1d3315cc60538ef4d3823951217190
Hokyeong Choi <dollhy@gmail.com>
2019-10-31 05:10:36 +0900

    위도 경도 바꾸기, sqlite table festival, restaurants 생성

---
+++ experiment/latlng.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+pathfrom = "데이터전처리/2015_축제_위경도 추가.csv"
+pathto = "experiment/2015_축제_위경도 추가_수정1.csv"
+
+df = pd.read_csv(pathfrom, encoding="utf-8")
+
+t = df['x'] < df['y']
+x = df['x'] * ~t + df['y'] * t
+y = df['x'] * t + df['y'] * ~t
+df['x'] = x
+df['y'] = y
+
+df.to_csv(pathto, encoding="utf-8")
