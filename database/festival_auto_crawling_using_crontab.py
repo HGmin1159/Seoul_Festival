20dd517b2e778edb2098f47ff1a72e8ab993d007
Hokyeong Choi <dollhy@gmail.com>
2019-12-08 21:10:38 +0900

    DB에 음식점추가

--- database/festival_auto_crawling_using_crontab.py
+++ database/festival_auto_crawling_using_crontab.py
@@ -343,3 +343,104 @@ for i in range(add_new_table.shape[0]):
 
 con.commit()
 con.close()
+
+# In[31]:
+
+# 음식점 정보 가져오기(카카오)
+
+# import sqlite3, requests
+from collections import OrderedDict
+
+def create_restaurants(c):
+    c.execute("DROP TABLE IF EXISTS RESTAURANT_INFO")
+    c.execute('''CREATE TABLE "RESTAURANT_INFO" (
+            "id" INTEGER PRIMARY KEY,
+            "place_name" TEXT,
+            "category_name" TEXT,
+            "category_group_code" TEXT,
+            "category_group_name" TEXT,
+            "phone" TEXT,
+            "address_name" TEXT,
+            "road_address_name" TEXT,
+            "x" REAL,
+            "y" REAL,
+            "place_url" TEXT
+    );''')
+
+def create_festival_restaurants(c):
+    c.execute("DROP TABLE IF EXISTS FESTIVAL_RESTAURANT")
+    c.execute('''CREATE TABLE FESTIVAL_RESTAURANT (
+        festival_id INTEGER,
+        restaurants_id INTEGER,
+        distance INTEGER,
+        FOREIGN KEY(festival_id) REFERENCES FESTIVAL_INFO(festival_id) ON DELETE SET NULL,
+        FOREIGN KEY(restaurants_id) REFERENCES RESTAURANT_INFO(id) ON DELETE SET NULL
+    )''')
+
+KEYS = ['id', 'place_name', 'category_name', 'category_group_code',
+        'category_group_name', 'phone', 'address_name', 'road_address_name',
+        'x', 'y', 'place_url', 'distance']
+
+def get_restaurant(lng, lat, page):
+    rsp = requests.get(url,
+                       params={"category_group_code": "FD6",
+                               'x': str(lng), 'y': str(lat),
+                               "radius": 1000, "page": page, "size": 15},
+                       headers={"Authorization": "KakaoAK " + apikey}
+                       )
+    if rsp.status_code != 200:
+        raise Exception(rsp.status_code)
+    return rsp.json()
+
+def db_session_add(doc, pk, c):
+    for d in doc:
+        d = OrderedDict((k, d[k]) for k in KEYS)
+        t = []
+        for k, v in d.items():
+            if k != "id" and k != "distance":
+                t.append(v)
+            elif k == "id":
+                t.append(int(v))
+                res_id = int(v)
+            else:
+                dist = int(v)
+        c.execute('INSERT INTO FESTIVAL_RESTAURANT VALUES (?,?,?)',
+                  (pk, res_id, dist))
+        c.execute(
+            'INSERT OR IGNORE INTO RESTAURANT_INFO VALUES (?,?,?,?,?,?,?,?,?,?,?)', tuple(t))
+
+def fetch_fes(c):
+    """
+    in: connection
+    out: list
+    """
+    c.execute("""SELECT festival_id, x, y 
+                FROM FESTIVAL_INFO
+                WHERE x > 0;""")
+    lst = [dict(zip(('id', 'x', 'y'), row)) for row in c]
+    return lst
+
+
+url = "https://dapi.kakao.com/v2/local/search/category.json"
+
+con = sqlite3.connect(db_path)
+
+cur = con.cursor()
+
+create_restaurants(cur)
+
+create_festival_restaurants(cur)
+
+for festival in fetch_fes(cur):
+    page = 1
+    resp = get_restaurant(festival['x'], festival['y'], page)
+    while not resp["meta"]["is_end"]:
+        doc = resp["documents"]
+        db_session_add(doc, festival['id'], cur)
+        page += 1
+        resp = get_restaurant(festival['x'], festival['y'], page)
+    doc = resp["documents"]
+    db_session_add(doc, festival['id'], cur)
+
+con.commit()
+con.close()
